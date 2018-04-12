91f15581e9be6d75e58f45aeb00163e78f35c212
Octavia Payne <oapayne@gmail.com>
2018-04-11 23:29:15 -0700

    completed pick_winner and run_raffle functions

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,25 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from random import choice
+from customers import get_customers_from_file
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+customers = get_customers_from_file("customers.txt")
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print "Tell {name} at {email} that they've won.".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        )
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    pick_winner(customers)
+
+run_raffle()
